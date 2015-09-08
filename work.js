3e875a86998fb4254fb81282d4c0c760f3b5aef0
Rohit Sehgal <rohitben@hotmail.com>
2015-09-08 13:30:36 -0500

    Update work.js

--- work.js
+++ work.js
@@ -1,1 +1,23 @@
-
+function encrypt(text, level){
+  var x = [];
+  for(var i = 0; i < text.length; i++){
+    x.push(text.charCodeAt(i));
+  }
+  console.log(x);
+  for(var j = 0; j < text.length; j++){
+    crypt(x);
+  }
+  console.log("Secret !keep!:");
+  console.log(x[x.length-1]);
+  x.splice(x.length-1, 1);
+  console.log(x);
+}
+function crypt(x){
+  var random = Math.floor((Math.random() * 10) + 1);
+  for(var j = 0; j < x.length; j++){
+    x[j]*= random;
+    
+  }
+  x.push(random);
+  console.log(x);
+}
